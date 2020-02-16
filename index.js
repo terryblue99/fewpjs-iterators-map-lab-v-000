f7c20e8b50b7485989462764101c4eafbf98b31e
Terry Blue <terryblue@comcast.net>
2020-02-16 21:42:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+function titleCase(str) {
+  return str.toLowerCase().split(' ').map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
+}
+
 const titleCased = () => {
   return tutorials.map(tutorial => titleCase(tutorial))
 }
