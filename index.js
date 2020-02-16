b2b1deca3aa1da9a94b9e86aa4962441a121b4df
Terry Blue <terryblue@comcast.net>
2020-02-16 21:46:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const tutorials = [
 ];
 
 function titleCase(str) {
-  return str.toLowerCase().split(' ').map(function(word) {
+  return str.split(' ').map(function(word) {
     return (word.charAt(0).toUpperCase() + word.slice(1));
   }).join(' ');
 }
